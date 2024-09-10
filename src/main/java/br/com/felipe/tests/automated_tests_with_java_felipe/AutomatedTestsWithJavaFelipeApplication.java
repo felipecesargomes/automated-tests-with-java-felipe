c4cc0f43f675ba4601942db826de9842dc3864f8
Felipe <www.felipecesargomes@gmail.com>
2024-09-10 20:42:37 -0300

    update: maven

---
+++ src/main/java/br/com/felipe/tests/automated_tests_with_java_felipe/AutomatedTestsWithJavaFelipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.tests.automated_tests_with_java_felipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTestsWithJavaFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTestsWithJavaFelipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipe/tests/automated_tests_with_java_felipe/AutomatedTestsWithJavaFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipe.tests.automated_tests_with_java_felipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestsWithJavaFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
