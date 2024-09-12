1cb24e2e973f7a5650688bca9d72a21a903b4443
Felipe <www.felipecesargomes@gmail.com>
2024-09-12 18:07:25 -0300

    feat:assertEquals and assertNotNull

--- src/test/java/br/com/felipe/math/SimpleMathTest.java
+++ src/test/java/br/com/felipe/math/SimpleMathTest.java
@@ -3,14 +3,22 @@ package br.com.felipe.math;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class SimpleMathTest {
 
     @Test
     void test() {
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
+        double firstNumber = 6.2D;
+        double secondNumber = 2.0D;
+        Double actual = math.sum(firstNumber, secondNumber);
         double expected = 8.2D;
-        Assertions.assertEquals(expected, actual);
+
+        assertEquals(expected, actual, () -> firstNumber + "+" + secondNumber + " did not produce expected result!");
+        assertEquals(8.2, actual, "The testSum() did not produce expected result!");
+        assertNotNull(actual);
     }
 
 }
